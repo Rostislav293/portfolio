e2d9b6568205257e1f1ac5dba87602a2227f63d3
Rostislav <mawa2909@icloud.com>
2025-03-03 19:04:13 +0200

    portfolio

---
+++ js/styles.js
@@ -0,0 +1,6 @@
+<script>
+    document.body.addEventListener('click', function() {
+        document.body.style.backgroundColor = 
+            document.body.style.backgroundColor === 'black' ? '#0d0d0d' : 'black';
+    });
+</script>
